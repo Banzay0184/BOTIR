99f0420cdd18291286d9cb8ce3309304bd42888c
Banzay0184 <89055626+Banzay0184@users.noreply.github.com>
2024-08-10 17:15:12 +0500

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = ' http://127.0.0.1:8000/api/v1';
+const API_URL = 'https://banzay.pythonanywhere.com/api/v1';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
