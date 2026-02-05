9aced99a1b19804cb35c8fada38fa2be63c13112
Banzay0184 <shakhzodabidov@gmail.com>
2026-02-05 10:33:08 +0500

    deploy 0.9

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -5,6 +5,8 @@ DEBUG = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://www.scclms.uz",
+    "https://scclms.uz",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
